7b20c97cb92d3a2bb7f9d425295720cd49be915d
Jimbles <j.avery.10@ucl.ac.uk>
2015-05-21 15:00:59 +0100

    updated to run full 0th order reconstruction on small cyl mesh

--- test/reconstruction_example2Jimmy.m
+++ test/reconstruction_example2Jimmy.m
@@ -1,13 +1,36 @@
-    tic
-    n_J = size(J,1);% J is the Jacobian matrix
-    nLambda=3000;
-    %SVD
-    [U,S,V] = svd(J,'econ');% Singular value decomposition
-    disp(sprintf('SVD done: %.2f min.',toc/60))
-    
-    %pick lambda searchspace
-    lambda = logspace(-20,-2,nLambda);
-    
-    %Do inversion
-    [X,cv_error] = tikhonov_CV(J,DV,lambda,n_J,U,S,V,1);% DV is the measurement.
-    disp(sprintf('X done: %.2f min.',toc/60));
+
+%these are located in resources subfolder of repo
+
+load('Cyl_Mesh'); %load the Mesh and the Jacobian (from fwd solver)
+load('Ex_Cyl_DV'); % load boundary voltages
+
+%%
+
+%display boundaries of mesh
+DisplayBoundaries(Mesh); 
+
+%create hex mesh and hex jacobian - 10mm elements
+[Mesh_hex,J_hex]=convert_fine2coarse(Mesh.Tetra,Mesh.Nodes,J,10e-3);
+
+
+tic
+
+%this is the number of folds used in the cross validation in tikhonov_CV,
+%here we are using the maximum number
+n_J = size(J_hex,1);% J is the Jacobian matrix
+nLambda=3000;
+%SVD
+[U,S,V] = svd(J_hex,'econ');% Singular value decomposition
+disp(sprintf('SVD done: %.2f min.',toc/60))
+
+%pick lambda searchspace
+lambda = logspace(-20,-2,nLambda);
+
+%Do inversion
+[X,cv_error] = tikhonov_CV(J_hex,DV,lambda,n_J,U,S,V,1);% DV is the measurement.
+disp(sprintf('X done: %.2f min.',toc/60));
+
+
+%% make vtk
+
+writeVTKcell_hex('CylTest',Mesh_hex.Hex,Mesh_hex.Nodes,X);
