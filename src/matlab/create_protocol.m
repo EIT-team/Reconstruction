251c9229dc700dd73e2529100744dcd4dd974aff
Jimbles <j.avery.10@ucl.ac.uk>
2016-02-05 13:17:20 +0000

    Added comments to create Protocol

--- src/matlab/create_protocol.m
+++ src/matlab/create_protocol.m
@@ -1,13 +1,21 @@
 function Protocol = create_protocol(pos)
 % pos : electrode positioning nx3
 
-    for (i=1:size(pos,1))
+
+%    Create weights for spanning tree based on vector distance between
+%    electrodes
+    for i=1:size(pos,1)
         w(:,i)=1./sqrt((pos(:,1)-pos(i,1)).^2+(pos(:,2)-pos(i,2)).^2+...
             (pos(:,3)-pos(i,3)).^2);
     end
+    
+    %find graph which connects the electrodes with biggest length
     T=tril(sparse(w));
     [ST,pred] = graphminspantree(T);
+    %convert into array
     [i,j,s]=find(ST);
+    %put in order 
     [A]=sortrows([s,i,j]);
+    %take only the node numbers
     Protocol = A(:,2:3);
 end
