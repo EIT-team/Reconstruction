0ef6fb30130ed2fbcca658742a347db6fbf82d8b
Jimbles <j.avery.10@ucl.ac.uk>
2016-06-30 18:06:04 +0100

    Started Paraview loading script. Python is all hardcoded at the moment

---
+++ src/python/example_settings.py
@@ -0,0 +1,20 @@
+
+Cmap_name = 'u'
+
+Cmap_title = 'The stuff'
+
+Cmap_max = 100
+Cmap_min = -100
+
+Threshold_Neg_Min = -100
+Threshold_Neg_Max = -50
+
+Threshold_Pos_Min = 25
+Threshold_Pos_Max = 40
+
+Do_Pos_Thres = True
+Do_Neg_Thres = True
+
+Background_Opacity = 0.1
+
+VTK_Filenames = 'E:\\Neonate2016\\Parallel\\Recon\\arm_new\\output\\p_seq9_1_22.vtk'
