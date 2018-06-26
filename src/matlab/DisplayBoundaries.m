bb5adfa21aaa1d1015a5aca34663c489b4198b31
Jimbles <j.avery.10@ucl.ac.uk>
2018-06-26 14:07:09 +0100

    Displayboundaries works with supersolver meshes

--- src/matlab/DisplayBoundaries.m
+++ src/matlab/DisplayBoundaries.m
@@ -1,22 +1,25 @@
 function [h,Triangle_Boundary,Nodes_Boundary]=DisplayBoundaries(Mesh)
-%display boundaries of mesh from Kirill
+% Displays the boundaries of the mesh
 
+% change fields if using supersolver mesh output
+if ~isfield(Mesh,'Tetra') && isfield(Mesh,'tri')
+    Mesh = renameStructField(Mesh,'tri','Tetra');
+end
+if ~isfield(Mesh,'Nodes') && isfield(Mesh,'vtx')
+    Mesh = renameStructField(Mesh,'vtx','Nodes');
+end
 
 [Mesh.Nodes,Mesh.Tetra]=removeisolatednode(double(Mesh.Nodes(:,1:3)),double(Mesh.Tetra(:,1:4)));
-
 
 trep = triangulation(Mesh.Tetra, Mesh.Nodes);
 [Triangle_Boundary, Nodes_Boundary] = freeBoundary(trep);
 h= trisurf(Triangle_Boundary, Nodes_Boundary(:,1), Nodes_Boundary(:,2), Nodes_Boundary(:,3));
 
 
-% facenb=faceneighbors(double(mesh.Tetra(:,1:4)), 'surface');
-% TR=TriRep(facenb, mesh.Nodes(:,1),mesh.Nodes(:,2),mesh.Nodes(:,3));
-%figure;
-% h=trimesh(TR);
 set(h,'EdgeColor',[0.3,0.3,0.3],'FaceColor','w');
 % set(h,'EdgeColor',[100,143,229]/256,'EdgeAlpha',0.5);
 % set(h,'EdgeAlpha',0.5);
 %set(h,'FaceColor','None');
 daspect([1,1,1]);
+end
 
