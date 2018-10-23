861bbe74c6cb2b04e669036eca614b7baa30e90c
Jimbles <j.avery.10@ucl.ac.uk>
2018-10-23 15:36:58 +0100

    write binary vtks by default - saves at least 30% file size

--- src/matlab/writeVTKcell.m
+++ src/matlab/writeVTKcell.m
@@ -1,15 +1,15 @@
-function writeVTKcell(filename,Tetra,Nodes,Data,DataName)
-% vtk export
+function writeVTKcell(filename,Tetra,Nodes,Data,DataName,Format)
+% writeVTKcell(filename,Tetra,Nodes,Data,DataName,Format)
 % creates a vtk-file filename.vtk containing simplicial mesh data (2- or 3d)
 % and additional cell data
 % Based on code by Daniel Peterseim, 2009-11-07
-
+%% Check inputs
 if size(Tetra,1) ~= size(Data,1)
     error('Size of data and hexes dont match');
 end
 
 if size(Tetra,2) ~= 4 && size(Tetra,2) ~= 3
-    error('Tetra not correct, should be numHex x 4(3d) 3(2d)');
+    error('Tetra not correct, should be numHex x 4(3d) or 3(2d)');
 end
 
 if size(Nodes,2) ~= 3 && size(Nodes,2) ~= 2
@@ -24,38 +24,83 @@ if exist('DataName','var') ==0 || isempty(DataName)
     DataName = 'Data';
 end
 
+if exist('Format','var') ==0 || isempty(Format)
+    Asciiflag=0;
+else
+    if strcmpi(Format,'ascii')
+        Asciiflag=1;
+    else
+        if strcmpi(Format,'binary')
+            Asciiflag=0;
+        else
+            error('format flag should be ascii or binary');
+        end
+    end
+end
+
+%% Calculate arrays to write
 [np,dim]=size(Nodes);
 [nt]=size(Tetra,1);
 celltype=[3,5,10];
 
-
-%ensure we ahve .vtk at the end but dont add it if already given
+POINTS=[Nodes zeros(np,3-dim)]';
+CELLS=[(dim+1)*ones(nt,1) Tetra-1]';
+CELL_TYPES=celltype(dim)*ones(nt,1);
+%% Write file
+%ensure we have .vtk at the end but dont add it if already given
 [PATHSTR,NAME] = fileparts(filename);
 fname=fullfile(PATHSTR,[NAME '.vtk']);
- 
+
+% Open file and write the header
 FID = fopen(fname,'w+');
-fprintf(FID,'# vtk DataFile Version 2.0\nUnstructured Grid Example\nASCII\n');
-fprintf(FID,'DATASET UNSTRUCTURED_GRID\n');
- 
-fprintf(FID,'POINTS %d float\n',np);
-s='%f %f %f \n';
-P=[Nodes zeros(np,3-dim)];
-fprintf(FID,s,P');
- 
-fprintf(FID,'CELLS %d %d\n',nt,nt*(dim+2));
-s='%d ';
-for k=1:dim+1
-    s=horzcat(s,{' %d'});
+fprintf(FID,'# vtk DataFile Version 2.0\n');
+fprintf(FID,'Tetrahedral Mesh Data\n');
+
+if Asciiflag
+    fprintf(FID,'ASCII\n');
+else
+    fprintf(FID, 'BINARY\n');
 end
-s=cell2mat(horzcat(s,{' \n'}));
-fprintf(FID,s,[(dim+1)*ones(nt,1) Tetra-1]');
- 
+fprintf(FID,'DATASET UNSTRUCTURED_GRID\n');
+
+% Write the points
+fprintf(FID,'POINTS %d float\n',np);
+if Asciiflag
+    s='%f %f %f \n';
+    fprintf(FID,s,POINTS);
+else
+    fwrite(FID, POINTS, 'float', 'b');
+end
+
+% Write the cell definitons as INTEGERS because they are indices
+fprintf(FID,'CELLS %d %d\n',nt,nt*(dim+2));
+if Asciiflag
+    s='%d ';
+    for k=1:dim+1
+        s=horzcat(s,{' %d'});
+    end
+    s=cell2mat(horzcat(s,{' \n'}));
+    fprintf(FID,s,CELLS);
+else
+    fwrite(FID,CELLS, 'int32', 'b');
+end
+
+% Write the integer cell types as INTEGERS because they are ID Types
 fprintf(FID,'CELL_TYPES %d\n',nt);
-s='%d\n';
-fprintf(FID,s,celltype(dim)*ones(nt,1));
- 
+if Asciiflag
+    s='%d\n';
+    fprintf(FID,s,CELL_TYPES);
+else
+    fwrite(FID,CELL_TYPES, 'int32', 'b');
+end
+
+% Write the data array as float
 fprintf(FID,'CELL_DATA %s\nSCALARS %s float 1\nLOOKUP_TABLE default\n',num2str(nt),DataName);
-s='%f\n';
-fprintf(FID,s,Data);
- 
+if Asciiflag
+    s='%f\n';
+    fprintf(FID,s,Data);
+else
+    fwrite(FID, Data, 'float', 'b');
+end
 fclose(FID);
+end
--- src/matlab/writeVTKcell_hex.m
+++ src/matlab/writeVTKcell_hex.m
@@ -1,6 +1,7 @@
-function writeVTKcell_hex(filename,Hexes,Nodes,Data,DataName)
+function writeVTKcell_hex(filename,Hexes,Nodes,Data,DataName,Format)
 % vtk export for Hex elements
 
+%% Check inputs
 if size(Hexes,1) ~= size(Data,1)
     error('Size of data and hexes dont match');
 end
@@ -17,40 +18,87 @@ if size(Data,2) > 1
     error('Too much data, should be numHex x 1');
 end
 
-[np,dim]=size(Nodes);
-[nt]=size(Hexes,1);
+
 
 if exist('DataName','var') ==0 || isempty(DataName)
     DataName = 'Data';
 end
 
-%ensure we ahve .vtk at the end but dont add it if already given
+if exist('Format','var') ==0 || isempty(Format)
+    Asciiflag=0;
+else
+    if strcmpi(Format,'ascii')
+        Asciiflag=1;
+    else
+        if strcmpi(Format,'binary')
+            Asciiflag=0;
+        else
+            error('format flag should be ascii or binary');
+        end
+    end
+end
+%% Calculate arrays to write
+[np,dim]=size(Nodes);
+[nt]=size(Hexes,1);
+
+POINTS=[Nodes zeros(np,3-dim)]';
+CELLS=[8*ones(nt,1) Hexes-1]'; % hexes need 8 points to define them (first col)
+CELL_TYPES=12*ones(nt,1); %hex is type 12
+%% Write file
+%ensure we have .vtk at the end but dont add it if already given
 [PATHSTR,NAME] = fileparts(filename);
 fname=fullfile(PATHSTR,[NAME '.vtk']);
 
-FID = fopen(fname,'w');
-fprintf(FID,'# vtk DataFile Version 2.0\nUnstructured Grid Example\nASCII\n');
+% Open file and write the header
+FID = fopen(fname,'w+');
+fprintf(FID,'# vtk DataFile Version 2.0\n');
+fprintf(FID,'Tetrahedral Mesh Data\n');
+
+if Asciiflag
+    fprintf(FID,'ASCII\n');
+else
+    fprintf(FID, 'BINARY\n');
+end
 fprintf(FID,'DATASET UNSTRUCTURED_GRID\n');
 
+% Write the points
 fprintf(FID,'POINTS %d float\n',np);
-s='%f %f %f \n';
-P=[Nodes zeros(np,3-dim)];
-fprintf(FID,s,P');
-
-fprintf(FID,'CELLS %d %d\n',nt,nt*9);
-s='%d ';
-for k=1:8
-    s=horzcat(s,{' %d'});
+if Asciiflag
+    s='%f %f %f \n';
+    fprintf(FID,s,POINTS);
+else
+    fwrite(FID, POINTS, 'float', 'b');
 end
-s=cell2mat(horzcat(s,{' \n'}));
-fprintf(FID,s,[8*ones(nt,1) Hexes-1]');
 
+% Write the cell definitons as INTEGERS because they are indices
+fprintf(FID,'CELLS %d %d\n',nt,nt*9);
+if Asciiflag
+    s='%d ';
+    for k=1:8
+        s=horzcat(s,{' %d'});
+    end
+    s=cell2mat(horzcat(s,{' \n'}));
+    fprintf(FID,s,CELLS);
+else
+    fwrite(FID,CELLS, 'int32', 'b');
+end
+
+% Write the cell definitons as INTEGERS because they are indices
 fprintf(FID,'CELL_TYPES %d\n',nt);
-s='%d\n';
-fprintf(FID,s,12*ones(nt,1));
+if Asciiflag
+    s='%d\n';
+    fprintf(FID,s,CELL_TYPES);
+else
+    fwrite(FID,CELL_TYPES, 'int32', 'b');
+end
 
+% Write the data array as float
 fprintf(FID,'CELL_DATA %s\nSCALARS %s float 1\nLOOKUP_TABLE default\n',num2str(nt),DataName);
-s='%f\n';
-fprintf(FID,s,Data);
-
+if Asciiflag
+    s='%f\n';
+    fprintf(FID,s,Data);
+else
+    fwrite(FID, Data, 'float', 'b');
+end
 fclose(FID);
+end
